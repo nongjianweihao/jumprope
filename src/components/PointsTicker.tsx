31bdc2dbafbd5cce984bd7c63a14926e45015385
nongjianweihao <nongjianweihao@gmail.com>
2025-10-08 23:30:19 +0800

    Delete src directory

--- src/components/PointsTicker.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  delta: number;
-}
-
-export function PointsTicker({ delta }: Props) {
-  const prefix = delta >= 0 ? '+' : '';
-  return <span className="text-lg font-black text-brand-secondary animate-bounce">{`${prefix}${delta}`}</span>;
-}
--- src/utils/report.ts
+++
@@ -1,3 +0,0 @@
-export async function generateReportDraft(studentName: string) {
-  return `【战报】${studentName} 在本节课表现突出，PR 再创新高！`;
-}
